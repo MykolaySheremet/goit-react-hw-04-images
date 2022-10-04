9d579ffcedf7240ad73b207ff9018a690fbaeb0a
MYKOLA SHEREMET <artem.vazovskin@gmail.com>
2022-10-04 20:24:31 +0300

    refactoring component SerchBar

--- src/components/Serchbar/Serchbar.jsx
+++ src/components/Serchbar/Serchbar.jsx
@@ -1,45 +1,37 @@
-import { Component } from "react";
+import { useState } from "react";
 import { toast } from 'react-toastify';
 import { SerchFormBox, Header, SearchFormButton,SearchFormInput } from './Serchbar.styled';
 import { MdOutlineFindInPage } from "react-icons/md";
 import PropTypes from 'prop-types';
 
+export const Serchbar = ({propSubmit}) => {
+    const [searchPictures, setSearchPictures] = useState('');
 
-export class Serchbar extends Component {
-    state={
-        searchPictures: '',
+    const handleInputChange = (e) => {
+
+        setSearchPictures(e.currentTarget.value.toLowerCase());
     }
 
-    handleNameChange = (e) => {
+    const handleNameChange = (e) => {
         e.preventDefault();
         
-        if (this.state.searchPictures.trim() === '') {
+        if (searchPictures.trim() === '') {
             return toast.error("Please input some name pictures to find ");
         }
-        
-        this.props.propSubmit(this.state.searchPictures);
-        // this.setState({ searchPictures: '' });
+        propSubmit(searchPictures);
     }
 
-
-    handleInputChange = (e) => {
-
-        this.setState({ searchPictures: e.currentTarget.value.toLowerCase()})
-    }
-
-
-    render() {
         return (
             <Header>
                 <SerchFormBox
-                    onSubmit={this.handleNameChange}>
+                    onSubmit={handleNameChange}>
                         <SearchFormButton type="submit">
                             <MdOutlineFindInPage size={40} />
                         </SearchFormButton>
 
-                        <SearchFormInput onChange={this.handleInputChange}
+                        <SearchFormInput onChange={handleInputChange}
                             type="text"
-                            value={this.state.searchPictures}
+                            value={searchPictures}
                             autoComplete="off"
                             autoFocus
                             placeholder="Search images and photos"
@@ -49,7 +41,53 @@ export class Serchbar extends Component {
             
         )
     }
-}
+
+
+// export class Serchbar extends Component {
+//     // state={
+//     //     searchPictures: '',
+//     // }
+
+//     // handleNameChange = (e) => {
+//     //     e.preventDefault();
+        
+//     //     if (this.state.searchPictures.trim() === '') {
+//     //         return toast.error("Please input some name pictures to find ");
+//     //     }
+        
+//     //     this.props.propSubmit(this.state.searchPictures);
+//     //     // this.setState({ searchPictures: '' });
+//     // }
+
+
+//     // handleInputChange = (e) => {
+
+//     //     this.setState({ searchPictures: e.currentTarget.value.toLowerCase()})
+//     // }
+
+
+//     // render() {
+//     //     return (
+//     //         <Header>
+//     //             <SerchFormBox
+//     //                 onSubmit={this.handleNameChange}>
+//     //                     <SearchFormButton type="submit">
+//     //                         <MdOutlineFindInPage size={40} />
+//     //                     </SearchFormButton>
+
+//     //                     <SearchFormInput onChange={this.handleInputChange}
+//     //                         type="text"
+//     //                         value={this.state.searchPictures}
+//     //                         autoComplete="off"
+//     //                         autoFocus
+//     //                         placeholder="Search images and photos"
+//     //                 />
+//     //             </SerchFormBox>
+//     //         </Header>
+            
+//     //     )
+//     // }
+// }
 
 Serchbar.propTypes = {
     propSubmit: PropTypes.func.isRequired,
