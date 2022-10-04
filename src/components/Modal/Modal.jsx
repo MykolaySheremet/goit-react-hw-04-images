58f86b41cdcf1dcabd6ba0590c05f6d0cb11317c
MYKOLA SHEREMET <artem.vazovskin@gmail.com>
2022-10-05 02:58:51 +0300

    change hooks in modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,49 +1,88 @@
-import { Component } from 'react';
 import { createPortal } from 'react-dom';
 import { BakcDrop, ModalWindow } from './Modal.styled';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
+export const Modal = ({ onClose, children }) => {
+    useEffect(() => {
+        const handlecloseEscape = (e) => {
 
-    componentDidMount() {
-        // console.log('did mount')
-        window.addEventListener('keydown', this.handlecloseEscape)
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handlecloseEscape)
-     }
-
-    handlecloseEscape = (e) => {
-        // console.log('Esc')
-        if (e.code === 'Escape') {
-            this.props.onClose();
+            if (e.code === 'Escape') {
+                onClose();
+            }
         }
-    }
 
-    closeModal = (e) => {
+        window.addEventListener('keydown', handlecloseEscape)
+    
+        return (() => { window.removeEventListener('keydown', handlecloseEscape) })
+    
+    },[onClose])
+
+
+
+
+    const closeModal = (e) => {
         if (e.target === e.currentTarget) {
-            this.props.onClose();
+            onClose();
         }
   }
 
 
-    render() {
-        return createPortal(
-            <BakcDrop onClick={this.closeModal}>
+
+    return createPortal(
+            <BakcDrop onClick={closeModal}>
                 <ModalWindow> 
-                    {this.props.children}
+                    {children}
                 </ModalWindow>
             </BakcDrop>,
             
             modalRoot)
-    }
-        
     
 }
+
+// export class Modal extends Component {
+
+    
+
+//     componentDidMount() {
+//         // console.log('did mount')
+//         window.addEventListener('keydown', this.handlecloseEscape)
+//     }
+
+//     componentWillUnmount() {
+//         window.removeEventListener('keydown', this.handlecloseEscape)
+//      }
+
+//     handlecloseEscape = (e) => {
+//         // console.log('Esc')
+//         if (e.code === 'Escape') {
+//             this.props.onClose();
+//         }
+//     }
+
+//     closeModal = (e) => {
+//         if (e.target === e.currentTarget) {
+//             this.props.onClose();
+//         }
+//   }
+
+
+    // render() {
+    //     return createPortal(
+    //         <BakcDrop onClick={this.closeModal}>
+    //             <ModalWindow> 
+    //                 {this.props.children}
+    //             </ModalWindow>
+    //         </BakcDrop>,
+            
+    //         modalRoot)
+    // }
+        
+    
+// }
 
 Modal.propTypes = {
     onClose: PropTypes.func.isRequired,
