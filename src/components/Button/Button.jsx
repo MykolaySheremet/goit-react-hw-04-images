c1b6b75eb85d91f3a155cc1328d1752ddbc5d4ba
MYKOLA SHEREMET <artem.vazovskin@gmail.com>
2022-10-06 16:40:58 +0300

    fix  description proptypes

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,19 @@
-import {Button} from './Button.styled'
+import { Button } from './Button.styled'
+import PropTypes from 'prop-types';
 
-export const ButtonMore = ({onClick }) => {
+export const ButtonMore = ({ onClick }) => {
+    // console.log(onClick)
     return <Button 
                 type='button'
                 onClick={onClick}>
                 Load More
            </Button>
+}
+
+
+
+
+Button.propTypes = {
+    onClick: PropTypes.func.isRequired,
 }
 
